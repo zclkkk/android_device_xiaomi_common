32e0662c73cef8074372b9deab91f228bc06db76
Bruno Martins <bgcngm@gmail.com>
2022-03-04 16:19:18 +0000

    interfaces: Split between vendors
    This should make updates-makefiles.sh usable.
    
    Change-Id: I118fcb1bcbed290eb894f92c058269189f2dde84

--- interfaces/updates-makefiles.sh
+++ interfaces/updates-makefiles.sh
@@ -3,4 +3,11 @@
 source $ANDROID_BUILD_TOP/system/tools/hidl/update-makefiles-helper.sh
 
 do_makefiles_update \
+<<<<<<< HEAD
   "vendor.xiaomi:device/xiaomi/common/interfaces"
+=======
+  "vendor.goodix:hardware/xiaomi/interfaces/goodix"
+
+do_makefiles_update \
+  "vendor.xiaomi:hardware/xiaomi/interfaces/xiaomi"
+>>>>>>> 1bb66f6 (interfaces: Split between vendors)
